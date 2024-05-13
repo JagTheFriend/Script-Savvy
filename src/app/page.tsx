4e2d9739845f8455fffbe99de2905a37598532dd
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-13 18:57:33 +0200

    Working on displaying post content
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "~/components/Navbar";
+import PostContents from "~/components/PostContents";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col text-white">
+    <main className="flex flex-col bg-base-100 text-white">
       <Navbar />
+      <PostContents />
     </main>
   );
 }
---
+++ src/components/PostContents.tsx
@@ -0,0 +1,8 @@
+export default function PostContents() {
+  return (
+    <section>
+      <h1>Post Contents</h1>
+      <p>This is the contents of the post.</p>
+    </section>
+  );
+}
