46c20046b38e86361b22fe1eb651f673d1941513
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-13 19:54:29 +0200

    Created auth page
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,24 @@
+import { SignIn, SignUp } from "@clerk/nextjs";
+
+export default function Page({
+  params: _params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams?: { type: "sign-in" | "sign-up" };
+}) {
+  const searchParam = searchParams?.type ?? "sign-in";
+  return (
+    <>
+      <h1 className="cursor-pointer text-2xl transition-all hover:underline">
+        Please {searchParam === "sign-in" ? "login" : "register"} to Script
+        Savvy
+      </h1>
+      {searchParam === "sign-in" ? (
+        <SignIn routing="hash" />
+      ) : (
+        <SignUp routing="hash" />
+      )}
+    </>
+  );
+}
