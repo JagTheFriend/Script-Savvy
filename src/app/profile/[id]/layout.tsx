b7c668de0d0df39ca981c5642ad28048e4ff52cb
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-14 15:26:45 +0200

    Created new route
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/app/profile/[id]/layout.tsx
@@ -0,0 +1,21 @@
+import Navbar from "~/components/Navbar";
+import "~/styles/globals.css";
+
+export const metadata = {
+  title: "Script Savvy",
+  description: "Simple Blog website created using create-t3-app",
+  icons: [{ rel: "icon", url: "/logo.svg" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex flex-col bg-base-100 text-white">
+      <Navbar />
+      {children}
+    </main>
+  );
+}
