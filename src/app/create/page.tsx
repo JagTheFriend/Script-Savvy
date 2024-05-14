4b4b6e05821e37d1276f13c12dff369971af9717
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-14 19:14:01 +0200

    Created simple form
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,3 +1,33 @@
+function SubmitButton() {
+  return (
+    <div className="flex flex-row items-center justify-center gap-4">
+      <button className="btn btn-outline btn-success">Post</button>
+      <button className="btn btn-outline btn-warning">Cancel</button>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <form className="flex flex-col items-center justify-center gap-4 p-4">
+      <input
+        type="text"
+        placeholder="Enter Title"
+        className="input input-bordered w-full lg:max-w-xs"
+      />
+      <textarea
+        placeholder="Content"
+        className="textarea textarea-bordered textarea-lg w-full"
+      />
+      <SubmitButton />
+    </form>
+  );
+}
+
 export default function CreatePost() {
-  return <div>Hello world</div>;
+  return (
+    <section className="flex flex-col">
+      <Form />
+    </section>
+  );
 }
