b8a3b901608a1c374c06e55ca8da77c7f82ab4c6
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-14 16:27:55 +0200

    Created server action in order to fetch user details without leaking it to client
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/lib/actions.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { clerkClient } from "@clerk/nextjs/server";
+
+export async function getUserById(userId: string) {
+  try {
+    const user = await clerkClient.users.getUser(userId);
+    return {
+      error: false,
+      message: "",
+      id: user.id,
+      username:
+        user.username ||
+        user.firstName ||
+        user.lastName ||
+        user.fullName ||
+        "Unknown",
+      image: user.imageUrl,
+    };
+  } catch (error) {
+    return {
+      error: true,
+      message: (error as Error).message ?? "Something went wrong!",
+    };
+  }
+}
