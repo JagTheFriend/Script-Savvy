7c401948be95486910beeaddf08108e9bdfc30f0
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-14 16:12:30 +0200

    Added go back button
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { clerkClient } from "@clerk/nextjs/server";
+import { clerkClient, type User } from "@clerk/nextjs/server";
+import Link from "next/link";
 
 export default async function ProfilePage({
   params,
@@ -6,25 +7,35 @@ export default async function ProfilePage({
   params: { id: string };
 }) {
   const { id: userId } = params;
+  let user: User;
 
   try {
-    const user = await clerkClient.users.getUser(userId);
-    console.log(user);
+    user = await clerkClient.users.getUser(userId);
   } catch (error: unknown) {
     if ((error as { message: string }).message === "Not Found") {
       return (
-        <section className="mt-10 flex cursor-pointer justify-center text-4xl">
-          <p className="transition-all hover:underline">User Not Found!</p>
+        <section className="mt-10 flex cursor-pointer flex-col justify-center gap-4">
+          <p className="text-4xl transition-all hover:underline">
+            User Not Found!
+          </p>
+          <Link className="btn btn-ghost" href="/">
+            Go Back
+          </Link>
         </section>
       );
     } else {
       return (
-        <section className="mt-10 flex cursor-pointer justify-center text-4xl">
-          <p className="transition-all hover:underline">Something went wrong, please try again later!</p>
+        <section className="mt-10 flex cursor-pointer flex-col justify-center gap-4">
+          <p className="text-4xl transition-all hover:underline">
+            Something went wrong!
+          </p>
+          <Link className="btn btn-ghost" href="/">
+            Go Back
+          </Link>
         </section>
-      )
+      );
     }
   }
 
-  return <section>Profile: {userId}</section>;
+  return <section>Profile: {user.firstName}</section>;
 }
