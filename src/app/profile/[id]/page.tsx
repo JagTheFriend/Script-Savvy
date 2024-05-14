70e5778d9ceb0ca68e9ed4a40f339bd6bd342bc6
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-14 16:28:08 +0200

    Use server action
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,10 +1,12 @@
-import { clerkClient, type User } from "@clerk/nextjs/server";
 import Link from "next/link";
+import { getUserById } from "~/lib/actions";
 
 function DisplayError({ text }: { text: string }) {
   return (
-    <section className="mt-10 flex cursor-pointer flex-col justify-center gap-4">
-      <p className="text-4xl transition-all hover:underline">{text}</p>
+    <section className="mt-10 flex flex-col items-center justify-center gap-4">
+      <p className="cursor-default text-4xl transition-all hover:underline">
+        {text}
+      </p>
       <Link className="btn btn-ghost" href="/">
         Go Back
       </Link>
@@ -18,17 +20,11 @@ export default async function ProfilePage({
   params: { id: string };
 }) {
   const { id: userId } = params;
-  let user: User;
+  const user = await getUserById(userId);
 
-  try {
-    user = await clerkClient.users.getUser(userId);
-  } catch (error: unknown) {
-    if ((error as { message: string }).message === "Not Found") {
-      return <DisplayError text="User not found" />;
-    } else {
-      return <DisplayError text="Something went wrong" />;
-    }
+  if (user.error) {
+    return <DisplayError text={user.message} />;
   }
 
-  return <section>Profile: {user.firstName}</section>;
+  return <section>Profile: {user.username}</section>;
 }
