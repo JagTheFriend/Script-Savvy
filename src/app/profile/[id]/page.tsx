8bd8021ef8ebf9dbb35b99045c7289cbeaefa21e
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-14 16:06:04 +0200

    Display error
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,8 +1,30 @@
-"use client";
+import { clerkClient } from "@clerk/nextjs/server";
 
-import { useParams } from "next/navigation";
+export default async function ProfilePage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id: userId } = params;
 
-export default function ProfilePage() {
-  const { id } = useParams<{ id: string }>();
-  return <section>Profile</section>;
+  try {
+    const user = await clerkClient.users.getUser(userId);
+    console.log(user);
+  } catch (error: unknown) {
+    if ((error as { message: string }).message === "Not Found") {
+      return (
+        <section className="mt-10 flex cursor-pointer justify-center text-4xl">
+          <p className="transition-all hover:underline">User Not Found!</p>
+        </section>
+      );
+    } else {
+      return (
+        <section className="mt-10 flex cursor-pointer justify-center text-4xl">
+          <p className="transition-all hover:underline">Something went wrong, please try again later!</p>
+        </section>
+      )
+    }
+  }
+
+  return <section>Profile: {userId}</section>;
 }
