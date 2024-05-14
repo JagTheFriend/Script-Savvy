7d623c8f1e3b8b2ab3ef3938672cc76928ea513a
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-14 15:31:55 +0200

    It can receive id from url
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
 export default function ProfilePage() {
+  const { id } = useParams<{ id: string }>();
   return <section>Profile</section>;
 }
