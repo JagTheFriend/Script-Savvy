466fa8350f4959e3d861e8349e244006f08028bf
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-14 19:28:25 +0200

    Created simple action
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
-import { clerkClient } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { db } from "~/server/db";
 
 export async function getUserById(userId: string) {
   try {
@@ -28,6 +29,32 @@ export async function getUserById(userId: string) {
       image: "",
       dateJoined: 0,
       email: "",
+    };
+  }
+}
+
+export async function createPost(formData: FormData) {
+  const title = formData.get("title") as string;
+  const content = formData.get("content") as string;
+  const { userId: authorId } = auth();
+
+  if (!authorId) {
+    return { error: true, message: "Unauthorized" };
+  }
+
+  try {
+    await db.post.create({
+      data: {
+        title,
+        content,
+        authorId,
+      },
+    });
+    return { error: false, message: "Post created" };
+  } catch (error) {
+    return {
+      error: true,
+      message: (error as Error).message ?? "Something went wrong!",
     };
   }
 }
