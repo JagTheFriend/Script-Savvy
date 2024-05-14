685cb19f603e2362de5e7a293a3b61bb06cd0e7f
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-14 17:18:17 +0200

    Break it into simpler components
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,69 @@
 import Link from "next/link";
 import { getUserById } from "~/lib/actions";
 
+function DateJoined({ dateJoined }: { dateJoined: number }) {
+  return (
+    <div className="flex cursor-default flex-row items-center gap-2">
+      <svg
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        stroke="white"
+        stroke-width="2"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+      </svg>
+      Joined on {new Date(dateJoined).toLocaleDateString()}
+    </div>
+  );
+}
+
+function EmailAddress({ email }: { email: string }) {
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+        <polyline points="22,6 12,13 2,6"></polyline>
+      </svg>
+      <Link href={`mailto:${email}`}>{email}</Link>
+    </div>
+  );
+}
+
+function ProfileImage({
+  image,
+  username,
+}: {
+  image: string;
+  username: string;
+}) {
+  return (
+    <div className="flex flex-row items-center justify-center gap-4">
+      <div className="avatar">
+        <div className="w-12 rounded-full">
+          <img src={image} />
+        </div>
+      </div>
+      <p className="cursor-default font-bold sm:text-2xl md:text-2xl lg:text-4xl">
+        {username}
+      </p>
+    </div>
+  );
+}
+
 function DisplayUsername(props: {
   username: string;
   image: string;
@@ -9,35 +72,10 @@ function DisplayUsername(props: {
 }) {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <div className="flex flex-row items-center justify-center gap-4">
-        <div className="avatar">
-          <div className="w-12 rounded-full">
-            <img src={props.image} />
-          </div>
-        </div>
-        <p className="text-2xl font-bold">{props.username}</p>
-      </div>
-      <div>
-        <div>Joined on {new Date(props.dateJoined).toLocaleDateString()}</div>
-        {props.email !== "unknown" && (
-          <div className="flex flex-row items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="white"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-              <polyline points="22,6 12,13 2,6"></polyline>
-            </svg>
-            <Link href={`mailto:${props.email}`}>{props.email}</Link>
-          </div>
-        )}
+      <ProfileImage image={props.image} username={props.username} />
+      <div className="flex flex-col justify-center gap-2">
+        <DateJoined dateJoined={props.dateJoined} />
+        {props.email !== "unknown" && <EmailAddress email={props.email} />}
       </div>
     </div>
   );
