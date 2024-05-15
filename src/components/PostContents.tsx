666c5041554dddda2f93b8385e26177879bfab2f
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-15 12:06:43 +0200

    New way to display cards
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/PostContents.tsx
+++ src/components/PostContents.tsx
@@ -1,24 +1,17 @@
-function Post() {
+import { Post } from "@prisma/client";
+import { db } from "~/server/db";
+
+function PostContent({ post }: { post: Post }) {
   return (
-    <div className="mx-auto max-w-lg">
-      <div className="mb-5 max-w-sm rounded-lg border border-gray-200 bg-white shadow-md">
-        <a href="#">
-          <img
-            className="rounded-t-lg"
-            src="https://flowbite.com/docs/images/blog/image-1.jpg"
-            alt=""
-          />
-        </a>
+    <div className="mx-auto w-full">
+      <div className="mb-5 rounded-lg border border-gray-200 bg-white shadow-md">
         <div className="p-5">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-              Noteworthy technology acquisitions 2021
+              {post.title}
             </h5>
           </a>
-          <p className="mb-3 font-normal text-gray-700">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
+          <p className="mb-3 font-normal text-gray-700">{post.content}</p>
           <a
             className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
             href="#"
@@ -31,15 +24,14 @@ function Post() {
   );
 }
 
-export default function PostContents() {
+export default async function PostContents() {
+  const posts = await db.post.findMany({ take: 10 });
+
   return (
-    <section className="mt-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+    <section className="mx-1.5 mt-4 flex flex-col gap-1">
+      {posts.map((post) => (
+        <PostContent key={post.id} post={post} />
+      ))}
     </section>
   );
 }
