2390673c863e5d31701ed6ab6e151ab4c5dbcb3b
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-15 12:21:44 +0200

    Fixed styling
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/PostContents.tsx
+++ src/components/PostContents.tsx
@@ -4,22 +4,20 @@ import { db } from "~/server/db";
 
 function PostContent({ post }: { post: Post }) {
   return (
-    <div className="mx-auto w-full">
-      <div className="mb-5 rounded-lg border border-gray-200 bg-white shadow-md">
-        <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-              {post.title}
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700">{post.description}</p>
-          <Link
-            className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
-            href={`/read/${post.id}`}
-          >
-            Read more
-          </Link>
-        </div>
+    <div className="w-full rounded-lg border border-gray-200 bg-white shadow-md">
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            {post.title}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700">{post.description}</p>
+        <Link
+          className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
+          href={`/read/${post.id}`}
+        >
+          Read more
+        </Link>
       </div>
     </div>
   );
@@ -29,7 +27,7 @@ export default async function PostContents() {
   const posts = await db.post.findMany({ take: 10 });
 
   return (
-    <section className="mx-1.5 mt-4 flex flex-col gap-1">
+    <section className="m-1.5 flex flex-col gap-2">
       {posts.map((post) => (
         <PostContent key={post.id} post={post} />
       ))}
