60961ea0499e8d764131689d70c4d2cca0fcf45e
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-15 20:13:39 +0200

    Display error
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/read/[postId]/page.tsx
+++ src/app/read/[postId]/page.tsx
@@ -1,8 +1,17 @@
+import DisplayError from "~/components/DisplayError";
+import { getPostContent } from "~/lib/actions";
+
 export default async function ReadPostPage({
   params,
 }: {
   params: { postId: string };
 }) {
   const { postId } = params;
-  return <>{postId}</>;
+  const returnedData = await getPostContent(postId);
+
+  if (returnedData.error) {
+    return <DisplayError />;
+  }
+
+  return <section className="mt-5 flex flex-col px-1">{postId}</section>;
 }
