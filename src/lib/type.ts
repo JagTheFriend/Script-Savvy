bdd2a06a41757af60f60708a5061a5b9c49f50c7
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-15 12:36:55 +0200

    Created util function
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -3,4 +3,5 @@ export type CustomUserType = {
   username: string;
   email: string;
   image: string;
-}
+  dateJoined: number;
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
+import { User } from "@clerk/nextjs/server";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { CustomUserType } from "./type";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getUserDetail(user: User): CustomUserType {
+  return {
+    id: user.id,
+    username:
+      user.username ??
+      user.firstName ??
+      user.lastName ??
+      user.fullName ??
+      "Unknown",
+    email: user.emailAddresses[0]?.emailAddress ?? "unknown",
+    image: user.imageUrl,
+    dateJoined: user.createdAt,
+  };
 }
