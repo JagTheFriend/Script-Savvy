2d4e83b59a6fd269875bf03139aeb1382ad9f545
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-15 20:14:31 +0200

    text align center
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/DisplayError.tsx
+++ src/components/DisplayError.tsx
@@ -3,7 +3,7 @@
 export default function DisplayError({ text }: { text?: string }) {
   return (
     <section className="mt-10 flex flex-col items-center justify-center gap-4">
-      <p className="cursor-default text-4xl transition-all hover:underline">
+      <p className="cursor-default text-center text-4xl transition-all hover:underline">
         {text ?? "Something went wrong!"}
       </p>
     </section>
