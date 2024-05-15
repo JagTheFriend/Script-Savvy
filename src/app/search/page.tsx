2fd153cbf61770c88949f051eba8d218d1c81889
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-15 20:33:36 +0200

    Display simple input field
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,7 +3,28 @@ export default function SearchPage({
   searchParams,
 }: {
   params: { slug: string };
-  searchParams?: { q: string };
+  searchParams: { q: string };
 }) {
-  return <>{searchParams?.q}</>;
+  const query = searchParams.q;
+  return (
+    <>
+      <section className="m-5 flex flex-col border-b border-gray-700 pb-4">
+        <label className="input input-bordered flex items-center gap-2">
+          <input type="text" className="grow" placeholder="Search" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+      </section>
+    </>
+  );
 }
