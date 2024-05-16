826da668ec6f48790e53be2d6b5959ff023219ba
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-16 19:49:11 +0200

    Added placeholder
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/PostContentInput.tsx
+++ src/components/PostContentInput.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
 export default function TiptapPostContentInput() {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "",
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        // Use a placeholder:
+        placeholder: "Content of Post",
+      }),
+    ],
     editorProps: {
       attributes: {
         class: "textarea input-bordered textarea-lg w-full lg:max-w-4xl",
