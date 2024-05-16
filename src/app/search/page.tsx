f28764cbc18e8c55353a7215d57caeffa2693138
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-16 10:44:48 +0200

    Display error message
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,23 @@
+import DisplayError from "~/components/DisplayError";
 import SearchResult from "~/components/SearchResult";
+import { getPostByQuery } from "~/lib/actions";
 
 export default async function SearchPage({
   searchParams,
 }: {
   searchParams: { q: string };
 }) {
+  const query = searchParams.q ?? "";
+
+  const data = await getPostByQuery(query);
+
   return (
     <>
       <SearchResult />
-      {searchParams.q}
+      {data.error && <DisplayError />}
+      {!data.data && query.length !== 0 && (
+        <DisplayError text="No Results Found" />
+      )}
     </>
   );
 }
