3bd1bda1d0af87cf889cbe04cf21db591943e42a
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-16 19:42:48 +0200

    Removed unused code
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/PostContentInput.tsx
+++ src/components/PostContentInput.tsx
@@ -3,7 +3,7 @@
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
-const Tiptap = () => {
+export default function TiptapPostContentInput() {
   const editor = useEditor({
     extensions: [StarterKit],
     content: "",
@@ -14,20 +14,5 @@ const Tiptap = () => {
     },
   });
 
-  return (
-    <EditorContent className="w-full lg:max-w-4xl" editor={editor} />
-  );
-};
-
-export default Tiptap;
-
-function PostContentInput() {
-  return (
-    <textarea
-      name="content"
-      placeholder="Enter Content"
-      className="textarea input-bordered textarea-lg w-full lg:max-w-4xl"
-      required
-    />
-  );
+  return <EditorContent className="w-full lg:max-w-4xl" editor={editor} />;
 }
