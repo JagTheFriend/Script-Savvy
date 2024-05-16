67815394409df21a77c6242e963674bf146f6d2f
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-16 19:32:03 +0200

    Created basic editor
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/PostContentInput.tsx
+++ src/components/PostContentInput.tsx
@@ -1,6 +1,25 @@
 "use client";
 
-export default function PostContentInput() {
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "",
+    editorProps: {
+      attributes: {
+        class: "textarea input-bordered textarea-lg w-full lg:max-w-4xl",
+      },
+    },
+  });
+
+  return <EditorContent className="w-full lg:max-w-4xl" editor={editor} />;
+};
+
+export default Tiptap;
+
+function PostContentInput() {
   return (
     <textarea
       name="content"
