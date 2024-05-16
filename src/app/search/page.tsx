7f2ad2cd125c89b1576ce78278e3a8c5d6e3fe30
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-16 10:36:04 +0200

    Added form to client component
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,61 +1,14 @@
-"use client";
+import SearchResult from "~/components/SearchResult";
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-
-export default function SearchPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [query, setQuery] = useState(searchParams.get("q") ?? "");
-
-  useEffect(() => {
-    if (query) {
-      router.push(`/search?${new URLSearchParams({ q: query })}`);
-    }
-  }, [query]);
-
+export default async function SearchPage({
+  searchParams,
+}: {
+  searchParams: { q: string };
+}) {
   return (
     <>
-      <div className="m-5">
-        <section className="flex flex-col border-b border-gray-700 pb-4">
-          <form
-            className="flex flex-col items-center justify-center gap-2"
-            onSubmit={(e) => {
-              e.preventDefault();
-              setQuery(inputRef.current?.value ?? "");
-            }}
-          >
-            <label className="input input-bordered flex items-center gap-2">
-              <input
-                name="query"
-                type="text"
-                className="grow"
-                placeholder="Search"
-                defaultValue={query}
-                ref={inputRef}
-                required
-                autoFocus
-              />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </label>
-            <button type="submit" className="btn btn-ghost btn-outline">
-              Search
-            </button>
-          </form>
-        </section>
-      </div>
+      <SearchResult />
+      {searchParams.q}
     </>
   );
 }
